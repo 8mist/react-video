4a687a88d01594627abe4bb93ce177eb1fafa1dd
Grégoire Ciles <bonjour@gregoireciles.fr>
2025-02-08 22:40:07 +0100

    refactor: update title

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Video } from '@/components/Video/Video';
 export const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center my-10">Meilisearch POC</h1>
-      <div className="container mb-20">
+      <h1 className="text-3xl font-bold underline text-center my-10">React Video Player</h1>
+      <div className="container">
         <Video src="/rick.mp4" />
       </div>
     </>
